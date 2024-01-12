47ec60eac94508108f39c2e2f3c41d664bcf8335
Trystan-Schmits <trystan.yt.2@gmail.com>
2024-01-12 21:29:55 +0000

    empty commit

--- node_backend/index.js
+++ node_backend/index.js
@@ -1,16 +1,24 @@
 const http = require('http');
 const server = http.createServer();
 const io = require('socket.io')(http,{cors: {origin: "*"}});
+const PORT = 3000;
 
 const { v4: uuidv4 } = require('uuid');
 
-const PORT = 3000;
+var leaderId;
+var idList = [];
 
 io.on('connection',(socket) => {
+
   const id = uuidv4();
   socket.emit("id",id);
-  
   console.log("a user connected, given id: "+id);
+
+  if(!leaderId){
+    leaderId = id
+    console.log("leader given id: "+id);
+  };
+  idList.push(id);
 
   socket.on('message',(message)=>{
       io.emit('message',message);
@@ -21,6 +29,11 @@ io.on('connection',(socket) => {
   })
   
   socket.on("disconnect", () => {
+      idList.splice(idList.indexOf(id),1);
+      if(id == leaderId){
+          leaderId = idList[0];
+          console.log("leader disconnected, leader given id"+idList[0]);
+      }
       console.log("a user disconnected, with id: "+id)
       io.emit("disconnection", id);
   })
