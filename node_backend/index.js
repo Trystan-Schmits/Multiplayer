49e88937d6f31074338da19b3e448f29911927f5
Trystan-Schmits <trystan.yt.2@gmail.com>
2024-01-16 21:25:14 +0000

    leader variable

--- node_backend/index.js
+++ node_backend/index.js
@@ -3,15 +3,36 @@ const { v4: uuidv4 } = require('uuid');
 
 const io = new Server({ cors: { origin: "*" } });
 
+var ids = [];
+var leader;
+
+function leaderFunc(id,userState){
+  if(userState == true){//user connecting
+    ids.push(id);
+    if(leader==undefined){
+      leader = id;
+    }
+  } 
+  else { //player disconnecting
+    ids.slice(ids.findIndex(id),1);
+    if(id == leader){
+      leader = ids[0];
+    }
+  }
+}
+
 io.on("connection", (socket) => {
-  const id = uuidv4()
-  socket.emit("id", id)
+  const id = uuidv4();
+  socket.emit("id", id);
+  leaderFunc(id,true);
+
   socket.on("update", (data) => {
     console.log(data)
     io.emit("stateUpdate", data)
   })
 
   socket.on("disconnect", () => {
+    leaderFunc(id,false);
     io.emit("disconnection", id)
   })
 });
